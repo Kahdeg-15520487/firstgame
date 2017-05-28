e53014be6b8b4fd877f6311ff8b07dbd9d296972
Kahdeg-15520487 <15520487@gm.uit.edu.vn>
2017-05-28 09:09:04 +0700

    part 5

--- www/app.js
+++ www/app.js
@@ -51,6 +51,20 @@ document.addEventListener(startEvent,function() {
 
 		ledge.body.immovable = true;
 
+		// The player and its settings
+		player = game.add.sprite(32, game.world.height - 150, 'dude');
+
+		//  We need to enable physics on the player
+		game.physics.arcade.enable(player);
+
+		//  Player physics properties. Give the little guy a slight bounce.
+		player.body.bounce.y = 0.4;
+		player.body.gravity.y = 200;
+		player.body.collideWorldBounds = true;
+
+		//  Our two animations, walking left and right.
+		player.animations.add('left', [0, 1, 2, 3], 10, true);
+		player.animations.add('right', [5, 6, 7, 8], 10, true);
 	}
 	
 	function update(){
