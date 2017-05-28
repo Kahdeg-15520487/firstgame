06adc09e211b5bb943fd2ff794297a7582e42ea3
Kahdeg-15520487 <15520487@gm.uit.edu.vn>
2017-05-28 09:03:52 +0700

    part 4

--- www/app.js
+++ www/app.js
@@ -34,22 +34,22 @@ document.addEventListener(startEvent,function() {
 		platforms.enableBody = true;
 
 		// Here we create the ground.
-		var ground = platforms.create(0, game.world.height - 64, 'ground');
+		//var ground = platforms.create(0, game.world.height - 64, 'ground');
 
 		//  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-		ground.scale.setTo(2, 2);
+		//ground.scale.setTo(2, 2);
 
 		//  This stops it from falling away when you jump on it
-		ground.body.immovable = true;
+		//ground.body.immovable = true;
 
 		//  Now let's create two ledges
-		var ledge = platforms.create(400, 400, 'ground');
+		//var ledge = platforms.create(400, 400, 'ground');
 
-		ledge.body.immovable = true;
+		//ledge.body.immovable = true;
 
-		ledge = platforms.create(-150, 250, 'ground');
+		//ledge = platforms.create(-150, 250, 'ground');
 
-		ledge.body.immovable = true;
+		//ledge.body.immovable = true;
 
 	}
 	
