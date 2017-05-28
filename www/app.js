70cc8a799a216594878abe496439cebe3c5ec75f
Kahdeg-15520487 <15520487@gm.uit.edu.vn>
2017-05-28 08:58:45 +0700

    second commit

--- www/app.js
+++ www/app.js
@@ -11,11 +11,19 @@ document.addEventListener(startEvent,function() {
 
     function preload () {
         game.load.image('logo', 'phaser.png');
+		game.load.image('sky', 'assets/sky.png');
+		game.load.image('ground', 'assets/platform.png');
+		game.load.image('star', 'assets/star.png');
+		game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
     }
 
     function create () {
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
         logo.anchor.setTo(0.5, 0.5);
     }
+	
+	function update(){
+		
+	}
 
 });
