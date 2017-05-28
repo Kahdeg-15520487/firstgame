e946e4c9650b740a66b2d54a68db8a038bfdf715
Kahdeg-15520487 <15520487@gm.uit.edu.vn>
2017-05-28 17:52:30 +0700

    tidy up the code

--- www/app.js
+++ www/app.js
@@ -1,15 +1,16 @@
-
 var startEvent = "DOMContentLoaded";
-if(window.cordova){
+if (window.cordova) {
     startEvent = "deviceready";
 }
-document.addEventListener(startEvent,function() {
+document.addEventListener(startEvent, function() {
 
 
-    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '',
-                                { preload: preload, create: create });
+    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', {
+        preload: preload,
+        create: create
+    });
 
-    function preload () {
+    function preload() {
         game.load.image('logo', 'phaser.png');
         game.load.image('sky', 'assets/sky.png');
         game.load.image('ground', 'assets/platform.png');
@@ -18,92 +19,86 @@ document.addEventListener(startEvent,function() {
     }
 
     var platforms;
-     var player;
+    var player;
 
-  function create() {
+    function create() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+        //  We're going to be using physics, so enable the Arcade Physics system
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+        //  A simple background for our game
+        game.add.sprite(0, 0, 'sky');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = game.add.group();
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+        //  We will enable physics for any object that is created in this group
+        platforms.enableBody = true;
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+        // Here we create the ground.
+        var ground = platforms.create(0, game.world.height - 64, 'ground');
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground.scale.setTo(2, 2);
 
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
+        //  This stops it from falling away when you jump on it
+        ground.body.immovable = true;
 
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 200, 'ground');
+        //  Now let's create two ledges
+        var ledge = platforms.create(400, 200, 'ground');
 
-    ledge.body.immovable = true;
+        ledge.body.immovable = true;
 
-    ledge = platforms.create(-100, 200, 'ground');
+        ledge = platforms.create(-100, 200, 'ground');
 
-    ledge.body.immovable = true;
+        ledge.body.immovable = true;
 
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+        // The player and its settings
+        player = game.add.sprite(32, game.world.height - 150, 'dude');
 
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
+        //  We need to enable physics on the player
+        game.physics.arcade.enable(player);
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
+        //  Player physics properties. Give the little guy a slight bounce.
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
 
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-  }
-
-  function update(){
-    //  Collide the player and the stars with the platforms
-    var hitPlatform = game.physics.arcade.collide(player, platforms);
-
-    var cursors = game.input.keyboard.createCursorKeys();
-
-    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown)
-    {
-      //  Move to the left
-      player.body.velocity.x = -150;
-
-      player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-      //  Move to the right
-      player.body.velocity.x = 150;
-
-      player.animations.play('right');
-    }
-    else
-    {
-      //  Stand still
-      player.animations.stop();
-
-      player.frame = 4;
+        //  Our two animations, walking left and right.
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
     }
 
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down && hitPlatform)
-    {
-      player.body.velocity.y = -350;
+    function update() {
+        //  Collide the player and the stars with the platforms
+        var hitPlatform = game.physics.arcade.collide(player, platforms);
+
+        var cursors = game.input.keyboard.createCursorKeys();
+
+        //  Reset the players velocity (movement)
+        player.body.velocity.x = 0;
+
+        if (cursors.left.isDown) {
+            //  Move to the left
+            player.body.velocity.x = -150;
+
+            player.animations.play('left');
+        } else if (cursors.right.isDown) {
+            //  Move to the right
+            player.body.velocity.x = 150;
+
+            player.animations.play('right');
+        } else {
+            //  Stand still
+            player.animations.stop();
+
+            player.frame = 4;
+        }
+
+        //  Allow the player to jump if they are touching the ground.
+        if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
+            player.body.velocity.y = -350;
+        }
     }
-  }
 
 });
